0179b663fe0215f0f45c18ed826d4dde9670d0e2
Mateus Souza <mota.mateus13@gmail.com>
2022-01-14 23:31:57 -0400

    Adicionando modificações no fronted
    - Novos campos para critérios de criação de senha
    - Responsividade dos campos
    - Javascript para controle de estados

--- frontend/static/new_password.js
+++ frontend/static/new_password.js
@@ -1,3 +1,19 @@
 (function () {
+  var allowLetterInput = document.querySelector("#allow_letters");
+  var allowUppercaseLettersInput = document.querySelector("#allow_uppercase_letters");
+  var allowLowecaseLettersInput = document.querySelector("#allow_lowercase_letters");
+  var letterVariation = document.querySelector("#letter_variation");
 
+  function unckeckLettersVariation() {
+    allowLowecaseLettersInput.checked = false;
+    allowUppercaseLettersInput.checked = false;
+    return true;
+  }
+
+  allowLetterInput.addEventListener("input", (el) => {
+    let allowLetters = el.target.checked;
+
+    allowLetters && letterVariation.classList.remove("d-none");
+    !allowLetters && unckeckLettersVariation() && letterVariation.classList.add("d-none");
+  });
 })();
